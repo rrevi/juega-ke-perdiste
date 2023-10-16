5af803962ae31b8624c104f94ac0219316bc0130
Rafael Revi <rafael.e.revi@gmail.com>
2023-10-16 19:44:54 -0400

    refactor of scr/pages/Home/index

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,50 +1,50 @@
+import { h } from 'preact';
 import { useCallback, useState } from 'preact/hooks';
 import './style.css';
 
 export function Home() {
 
 	const [gameHands, setGameHands] = useState([])
-	const [themScore, setThemScore] = useState(0)
-	const [usScore, setUsScore] = useState(0)
+	const [themTotalScore, setThemTotalScore] = useState(0)
+	const [usTotalScore, setUsTotalScore] = useState(0)
 	
 	const addHand = useCallback((them, us) => {
 		gameHands.push({them: parseInt(them), us: parseInt(us)})
 		setGameHands(gameHands)
 		setGameScore();
-		document.getElementById("them").value = ""
-		document.getElementById("us").value = ""
+		document.getElementById("themHandScore")["value"] = ""
+		document.getElementById("usHandScore")["value"] = ""
 	}, [])
 
 	function setGameScore() {
 		let themScore = 0
 		let usScore = 0
 		gameHands.forEach(hand => { themScore += hand.them; usScore += hand.us })
-		setThemScore(themScore)
-		setUsScore(usScore)
+		setThemTotalScore(themScore)
+		setUsTotalScore(usScore)
 	}
 
 	function addHandButtonClick(e) {
-		let themInputScore = getThemScore() ? getThemScore() : 0
-		let usInputScore = getUsScore() ? getUsScore() : 0
-		console.log("themInputScore: " + themInputScore)
-		console.log("usInputScore: " + usInputScore)
-		if(themInputScore > 0 || usInputScore > 0) {
-			addHand(themInputScore, usInputScore)
+		let themHandScore = getThemHandScore() ? getThemHandScore() : 0
+		let usHandScore = getUsHandScore() ? getUsHandScore() : 0
+
+		if(themHandScore > 0 || usHandScore > 0) {
+			addHand(themHandScore, usHandScore)
 		}
 	}
 
 	function newGameButtonClick(e) {
 		gameHands.length = 0
-		setThemScore(0)
-		setUsScore(0)
+		setThemTotalScore(0)
+		setUsTotalScore(0)
 	}
 
-	function getThemScore() {
-		return document.getElementById("them").value
+	function getThemHandScore() {
+		return document.getElementById("themHandScore")["value"]
 	}
 
-	function getUsScore() {
-		return document.getElementById("us").value
+	function getUsHandScore() {
+		return document.getElementById("usHandScore")["value"]
 	}
 
 	return (
@@ -56,13 +56,20 @@ export function Home() {
 							<th>Them</th>
 							<th>Us</th>
 							<th>
-								<button type="submit" class="newGameButton" onClick={newGameButtonClick}>&#8635;</button>
+								<button
+									type="submit"
+									id="newGameButton"
+									class="newGameButton"
+									onClick={newGameButtonClick}
+									title="New Game">
+										&#8635;
+								</button>
 							</th>
 						</tr>
 					</thead>
 					<tbody>
 						{gameHands.map(hand => (
-						<tr>
+						<tr class="handRow">
 							<td>{hand.them}</td>
 							<td>{hand.us}</td>
 							<td></td>
@@ -70,20 +77,41 @@ export function Home() {
 						))}
 						<tr>
 							<td>
-								<input type="number" id="them" name="them" placeholder="0" min="0" max="168" />
+								<input
+									type="number"
+									id="themHandScore"
+									name="themHandScore"
+									placeholder="0"
+									min="0"
+									max="168"
+									title="Them Hand Score" />
 							</td>
 							<td>
-								<input type="number" id="us" name="us" placeholder="0" min="0" max="168" />
+								<input
+									type="number"
+									id="usHandScore"
+									name="usHandScore"
+									placeholder="0"
+									min="0"
+									max="168"
+									title="Us Hand Score" />
 							</td>
 							<td>
-								<button type="submit" class="addHandButton" onClick={addHandButtonClick}>+</button>
+								<button
+									type="submit"
+									id="addHandButton"
+									class="addHandButton"
+									onClick={addHandButtonClick}
+									title="Add Hand">
+										+
+								</button>
 							</td>
 						</tr>
 					</tbody>
 					<tfoot>
 					<tr>
-						<th>{themScore}</th>
-						<th>{usScore}</th>
+						<th id="themTotalScore" title="Them Total Score">{themTotalScore}</th>
+						<th id="usTotalScore" title="Us Total Score">{usTotalScore}</th>
 						<th></th>
 					</tr>
 					</tfoot>

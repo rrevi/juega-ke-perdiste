389fc1395ae667c3c0c7171153222f7d97ff4642
rrevi <primers.flowing-0q@icloud.com>
2026-06-09 18:09:47 -0400

    ♻️ update the 404 component from class to pure function

--- src/pages/_404.jsx
+++ src/pages/_404.jsx
@@ -1,12 +1,8 @@
-import { h, Component } from 'preact';
-
-export default class NotFound extends Component {
-	render() {
-		return (
-			<section>
-				<h1>404: Not Found</h1>
-				<p>It's gone :(</p>
-			</section>
-		);
-	}
+export default function NotFound() {
+	return (
+		<section>
+			<h1>404: Not Found</h1>
+			<p>It's gone :(</p>
+		</section>
+	);
 }

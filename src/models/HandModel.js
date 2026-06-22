afdbc07818cbf903897b2a7b0552de284742e983
rrevi <primers.flowing-0q@icloud.com>
2026-06-21 20:41:18 -0400

    ♻️ removed use of couchdb, now using localStorage

--- src/models/HandModel.js
+++ src/models/HandModel.js
@@ -1,26 +1,18 @@
-import PouchDB from 'pouchdb';
-
 export default class HandModel {
   constructor(dbName) {
     this.dbName = dbName;
     this.hands = [];
     this.onChanges = [];
-
-    this.localDB = new PouchDB(this.dbName);
-    this.changesFeed = this.localDB.changes({
-      since: 'now',
-      live: true
-    }).on('change', () => {
-      this.draw();
-    }).on('error', () => {
-    });
-
     this.draw();
   }
 
-  async draw() {
-    const { rows } = await this.localDB.allDocs({ include_docs: true });
-    this.hands = rows.map(row => row.doc);
+  draw() {
+    try {
+      const stored = localStorage.getItem(this.dbName);
+      this.hands = stored ? JSON.parse(stored) : [];
+    } catch {
+      this.hands = [];
+    }
     this.inform();
   }
 
@@ -32,44 +24,47 @@ export default class HandModel {
   }
 
   inform() {
-    this.onChanges.forEach( cb => cb() );
+    this.onChanges.forEach(cb => cb());
   }
 
-  async add(themScore, usScore) {
+  add(themScore, usScore) {
     const hand = {
       _id: `${Date.now()}-${Math.random().toString(36).slice(2)}`,
-      "themScore": themScore,
-      "usScore": usScore
+      themScore,
+      usScore
     };
-
-    await this.localDB.put(hand);
+    this.hands.push(hand);
+    this.persist();
+    this.inform();
   }
 
-  async destroy(hand) {
-    await this.localDB.remove(hand);
-    this.hands = this.hands.filter( h => h !== hand);
+  destroy(hand) {
+    this.hands = this.hands.filter(h => h._id !== hand._id);
+    this.persist();
+    this.inform();
   }
 
-  async destroyAll() {
-    const deleted_hands = this.hands.map(hand => {
-      return Object.assign({}, hand, { _deleted: true });
-    });
-    await this.localDB.bulkDocs(deleted_hands);
+  destroyAll() {
     this.hands = [];
+    this.persist();
+    this.inform();
+  }
+
+  persist() {
+    localStorage.setItem(this.dbName, JSON.stringify(this.hands));
   }
 
   dispose() {
-    if (this.changesFeed) {
-      this.changesFeed.cancel();
-      this.changesFeed = null;
-    }
     this.onChanges = [];
   }
 
   totalScores() {
     let themTotalScore = 0;
-		let usTotalScore = 0;
-    this.hands.forEach(hand => { themTotalScore += hand.themScore; usTotalScore += hand.usScore });
+    let usTotalScore = 0;
+    this.hands.forEach(hand => {
+      themTotalScore += hand.themScore;
+      usTotalScore += hand.usScore;
+    });
     return [themTotalScore, usTotalScore];
   }
 }
